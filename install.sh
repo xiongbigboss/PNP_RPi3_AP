5d554cffb7582942261d550b039c5075bbac2127
xiongbigboss <13693421942@163.com>
2016-12-10 13:45:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #1. Install the necessary software------------------------------
-sudo apt-get update
+
 sudo apt-get install hostapd udhcpd -y
 sudo apt-get install iptables -y
 sudo apt-get install zip unzip -y
-sudo apt-get update
+
 #2. Configure DHCP----------------------------------------------
 x=tem.tem
 touch $x
